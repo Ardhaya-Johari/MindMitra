86abc171089bb135069d94ec67dc79354f824da1
Ardhaya Johari <ardhayasaxena3897@gmail.com>
2026-02-08 16:30:34 +0530

    Update favicon

--- start_project.bat
+++ start_project.bat
@@ -3,7 +3,7 @@ REM Activate Python virtual environment
 call venv\Scripts\activate
 
 REM Start FastAPI backend in a new terminal
-start cmd /k "cd backend && uvicorn main:app --reload"
+start cmd /k "cd backend && python -m uvicorn main:app --reload"
 
 REM Start React frontend in another terminal
 start cmd /k "cd frontend && npm start"
